456812f7c7932ec9dee977d4b0e04fca7ad48155
shanuba <104367849+shanuba@users.noreply.github.com>
2022-05-08 23:12:11 -0700

    Add files via upload

---
+++ twitter.js
@@ -0,0 +1,18 @@
+function myFunction(){
+let text=document.getElementById("input").value.length;
+
+    let number=document.getElementById("output");
+    number.innerHTML=text;
+
+}
+   
+
+     
+        
+    
+   
+    
+
+
+
+ 
